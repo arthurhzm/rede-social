6172ddc8b91bd29e2741770a6ebd3e3817284a4e
Arthur Marena <arthurzhm2@gmail.com>
2024-11-07 14:30:54 -0300

    WIP: página de login

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Main from './pages/main-page.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/login-page.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Main />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ front/src/pages/login-page.tsx
@@ -0,0 +1,12 @@
+import { Container, Stack } from "react-bootstrap";
+
+export default function LoginPage(){
+    return (
+        <Container fluid>
+            <Stack>
+                <h1>Bem vindo de volta!</h1>
+            </Stack>
+            
+        </Container>
+    )
+}
