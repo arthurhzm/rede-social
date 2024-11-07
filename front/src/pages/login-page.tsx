7a8cab5c09754cf488752aca6fc77925e482b861
Arthur Marena <arthurzhm2@gmail.com>
2024-11-07 14:42:22 -0300

    WIP: Login Form

--- front/src/pages/login-page.tsx
+++ front/src/pages/login-page.tsx
@@ -1,12 +1,41 @@
-import { Container, Stack } from "react-bootstrap";
+import { Col, Container, FloatingLabel, FormControl, InputGroup, Row, Stack } from "react-bootstrap";
 
-export default function LoginPage(){
+function LoginForm() {
+    return (
+        <>
+            <Row className="mb-2">
+                <Col md={3}>
+                    <InputGroup>
+                        <InputGroup.Text>@</InputGroup.Text>
+                        <FloatingLabel label="Usuário">
+                            <FormControl type="text" />
+                        </FloatingLabel>
+                    </InputGroup>
+                </Col>
+            </Row>
+            <Row className="mb-2">
+                <Col md={3}>
+                    <InputGroup>
+                        <FloatingLabel label="Senha">
+                            <FormControl type="password" />
+                        </FloatingLabel>
+                        <InputGroup.Text>
+                            olho
+                        </InputGroup.Text>
+                    </InputGroup>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default function LoginPage() {
     return (
         <Container fluid>
             <Stack>
                 <h1>Bem vindo de volta!</h1>
             </Stack>
-            
+            <LoginForm />
         </Container>
     )
 }
