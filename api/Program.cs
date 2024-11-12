58c823f90181ab80d1c6e877949c39b480791152
Arthur Marena <arthurzhm2@gmail.com>
2024-11-12 07:37:04 -0300

    FIX: DateTime no banco

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddCors(options =>
 {
